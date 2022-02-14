79a2cf5d997027c533863f94f53f648b23ff4bea
AMBLCO <matis.berubelauziere@gmail.com>
2022-02-14 13:58:12 -0500

    Initial commit

---
+++ DetailAlbedoSwitch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace HF_AMBLCO_Custom_Scripts
+{
+
+	public class DetailAlbedoSwitch : MonoBehaviour
+	{
+		public Texture DetailAlbedo;
+		public string MaterialName;
+		// Use this for initialization
+		void Update()
+		{
+			Renderer[] meshRenderer = GetComponentsInChildren<Renderer>();
+			if (meshRenderer.Length == 0)
+			{
+				return;
+			}
+
+			// Set the new material on the GameObject
+			for (int i = 0; i < meshRenderer.Length; i++)
+			{
+				var materials = meshRenderer[i].materials;
+				for (int j = 0; j < materials.Length; j++)
+				{
+					if (materials[j].name.Contains(MaterialName))
+					{
+						materials[j].SetTexture("_DetailAlbedoMap", DetailAlbedo);
+					}
+				}
+				meshRenderer[i].materials = materials;
+			}
+			this.enabled = false;
+		}
+	}
+}
---
+++ MeshSwitch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace HF_AMBLCO_Custom_Scripts
+{
+
+	public class MeshSwitch : MonoBehaviour
+	{
+		public Mesh Mesh;
+		public string MeshName;
+		void Update()
+		{
+			MeshFilter[] meshes = GetComponentsInChildren<MeshFilter>();
+			if (meshes.Length == 0)
+			{
+				return;
+			}
+
+			for (int i = 0; i < meshes.Length; i++)
+			{
+				Mesh mesh = meshes[i].mesh;
+				{
+					if (mesh.name.Contains(MeshName))
+					{
+						mesh = (Mesh);
+					}
+				}
+				meshes[i].mesh = mesh;
+			}
+			this.enabled = false;
+		}
+	}
+}
